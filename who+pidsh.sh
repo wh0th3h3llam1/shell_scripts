1839a3e5902df9c5728fc6ffea038cdb62881d1c
wh0th3h3llam1 <48444630+wh0th3h3llam1@users.noreply.github.com>
2019-04-07 10:42:58 +0530

    Add files via upload

---
+++ who+pidsh.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Author : Aarsh Sheth
+
+# No. of Counts 
+count=1
+
+# Get Username of the User
+printf 'Welcome '
+echo ''$USER
+
+printf "Enter Delay in Seconds (Default Value is 1 Sec): "
+read delay
+	
+# Check if user input is null.
+# If null, then it will assign 1 as default to delay variable.
+if [ -z $delay ]
+then
+	delay=1
+fi
+
+clear
+
+while(true)
+do
+	echo ''
+	echo 'Count = #'$count
+	who
+	
+	# Fires the who command and awk command extracts the last line from it.
+	# rev command reverses the string and cuts it till it encounters the first opening parenthesis.
+	# Then again reverse the string and again cut till closing parenthesis is encountered.
+	# This method gets the ip address of the connected/logged in user. 
+	ip="$( who | awk '/./{line=$0} END{print line}' | rev | cut -d '(' -f 1 | rev | cut -d ')' -f 1 )"
+	
+	# This is the regex (regular expression) pattern for validating ip address.
+	# If a valid ip is found, the condition will be true and it will get the pid (process id) of the ssh process.
+	# Then it will print the pid and kill the process.
+	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];
+	then
+		output=$(pidof sshd)
+		echo $output
+		echo ""
+		printf 'PID Of the Logged in User is : '
+		x="$( echo $output | cut -d ' ' -f 1 )"
+		echo $x
+		kill -9 $x
+	fi
+	
+	sleep $delay;
+
+	# Increase the count value
+	count=`expr $count + 1`
+done
+
+
+# Different Ways to check if variable is null
+	#if [ -z $delay ]
+	#if [ $delay -eq  0 ]
+	#if [ "$#" -eq "0" ]
+	#if test -z "$delay" 
+
+#wh0am1
